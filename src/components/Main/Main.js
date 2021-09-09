041f9548880882de41f8b1e0ae93af96f34186cc
Dasha V <dariavolyn@yandex.ru>
2021-09-08 21:40:50 -0400

    Main content

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import background from '../../images/main-background-phone.jpeg'
 
 function Main(props) {
     return (
         <div className="main">
-            <img className="main-background-image" src={background} alt="Person holding a phone"></img>
+            <div className="main-container">
+                <div className="main-title-container">
+                    <h1 className="main-title">What's going on in the world?</h1>
+                    <h2 className="main-subtitle">Find the latest news on any topic and save them in your personal account.</h2>
+                </div>
+                <form className="main-search">
+                    <input className="main-search-bar" type="text" id="search" placeholder="Enter topic" />
+                    <input className="button main-search-submit" type="submit" value="Search" />
+                </form>
+            </div>
         </div>
     )
 }
